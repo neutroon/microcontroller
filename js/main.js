6a030714ff9c1bc5a7214bbd46cd5df9a78995db
neutroon <191900312@ecu.edu.eg>
2023-12-09 13:34:23 +0200

    #

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+ $('button.up').click(moveForward)
+ $('button.left').click(moveLeft)
+ $('button.right').click(moveRight)
+ $('button.backward').click(moveBackward)
+
+
+
+
+
+
+function moveForward() { 
+    console.log('car is moving forward');
+ }
+function moveLeft() { 
+    console.log('car is moving left');
+ }
+function moveRight() { 
+    console.log('car is moving right');
+ }
+function moveBackward() { 
+    console.log('car is moving backward');
+ }
